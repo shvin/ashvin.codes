29ef291e4bbb6cb07acb532a2d8efcc3983f29e3
shvin <ashvin0119@gmail.com>
2022-02-04 20:38:16 -0500

    added paragraph section comp and index.js aboutme

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b8edc08c1f923a25"; }
+/******/ 		__webpack_require__.h = function() { return "c0ac1c9d070dde71"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.js
+++ pages/index.js
@@ -1,37 +1,71 @@
-import { Container, Box, Heading, Image, useColorModeValue } from '@chakra-ui/react'
+import {
+    Container,
+    Box,
+    Heading,
+    Image,
+    Link,
+    useColorModeValue
+} from '@chakra-ui/react'
+import NextLink from 'next/link'
+import Section from '../components/section'
+import Para from '../components/para'
 
 const Page = () => {
     return (
         <Container>
-            <Box borderRadius="lg" bg={useColorModeValue('#946bfa', '#946bfa')} p={3} mb={6} align="center">
+            <Box
+            borderRadius="lg"
+            bg={useColorModeValue('#946bfa', '#946bfa')}
+            p={3}
+            mb={6}
+            align="center"
+            >
                 hello ~ welcome to ashvin.codes!
             </Box>
 
-            <Box display={{md: 'flex'}}>
+            <Box display={{ md: 'flex' }}>
                 <Box flexGrow={1}>
                     <Heading as="h2" variant="page-title">
                         Ashvin Ramanathan
                     </Heading>
+                <p>Internet Enthusiast [Student / Developer]</p>
                 </Box>
-            <p>Internet Enthusiast <br /> [Student / Developer]</p>
+                <Box
+                flexShrink={0}
+                mt={{ base: 4, md: 0 }}
+                ml={{ md: 6 }}
+                align="center"
+                >
+                    <Image
+                    borderColor="whiteAlpha.800"
+                    borderWidth={2}
+                    borderStyle="solid"
+                    maxWidth="100px"
+                    display="inline-block"
+                    borderRadius="full"
+                    src="/images/ashvin.jpg"
+                    alt="Profile Picture"
+                    />
+                </Box>
             </Box>
-            <Box
-            flexShrink={0}
-            mt={{ base: 4, md: 0 }}
-            ml={{ md: 6 }}
-            align="center"
-            >
-                <Image
-                borderColor="whiteAlpha.800"
-                borderWidth={2}
-                borderStyle="solid"
-                maxWidth="100px"
-                display="inline-block"
-                borderRadius="full"
-                src="/images/ashvin.jpg"
-                alt="Profile Picture"
-                />
-            </Box>
+
+            <Section delay={0.1}>
+                <Heading as="h3" variant="section-title">
+                    about
+                </Heading>
+                <Para>
+                    Ashvin is a student at the University of Ottawa studying Computer Science. 
+                    Looking for internship opportunities in the USA(Atlanta) and Canada(Ottawa).
+                    You can find my recent work, posts, and other things ashvin on this site.
+                    My most recent project is this website {' '}
+                    <NextLink href="/works/ashvincodes">
+                        <Link>
+                            ashvin.codes 
+                        </Link>
+                    </NextLink>
+                    ; built with Next.js, Chakra UI, Framer Motion, and Three.js. 
+                </Para>
+            </Section>
         </Container>
     )
 }
